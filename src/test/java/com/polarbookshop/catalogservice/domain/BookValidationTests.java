3a7106f7a2b3a56dde8d06080a79a639c203d409
George Botos <george.botos.no1@gmail.com>
2023-11-03 11:57:35 +0000

    BookValidationTests -> updated to all caeses

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +26,7 @@ public class BookValidationTests {
 
 	@Test
 	@DisplayName("When all fields are correct, then validation should succeed.")
-	void validateAllFields() {
+	void allFieldsCorrectValidationTest() {
 		var book = Book.builder()
 		               .isbn("1234567890")
 		               .title("Title")
@@ -37,8 +38,23 @@ public class BookValidationTests {
 	}
 
 	@Test
+	@DisplayName("When ISBN is null, then validation should fail.")
+	void missingIsbnValidationTest() {
+		var book = Book.builder()
+		               .title("Title")
+		               .author("Author")
+		               .price(9.9)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book ISBN must be defined.");
+	}
+
+	@Test
 	@DisplayName("When ISBN defined but it is incorrect, then validation should fail.")
-	void validateInvalidIsbn() {
+	void invalidIsbnValidationTest() {
 		var book = Book.builder()
 		               .isbn("a234567890")
 		               .title("Title")
@@ -50,5 +66,103 @@ public class BookValidationTests {
 		assertThat(violations.iterator()
 		                     .next()
 		                     .getMessage()).isEqualTo("The ISBN format must be valid: a 10 or 13 digit number.");
+	}
+
+	@Test
+	@DisplayName("When ISBN is not defined, then validation should fail.")
+	void notDefinedIsbnValidationTest() {
+		var book = Book.builder()
+		               .isbn("")
+		               .title("Title")
+		               .author("Author")
+		               .price(9.9)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(2);
+		List<String> constrainViolationMessages = violations.stream()
+		                                                    .map(ConstraintViolation::getMessage)
+		                                                    .toList();
+		assertThat(constrainViolationMessages).contains("The book ISBN must be defined.")
+		                                      .contains("The ISBN format must be valid: a 10 or 13 digit number.");
+	}
+
+	@Test
+	@DisplayName("When title is not defined, then validation should fail.")
+	void notDefinedTitleValidationTest() {
+		var book = Book.builder()
+		               .isbn("1234567890")
+		               .title("")
+		               .author("Author")
+		               .price(9.9)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book title must be defined.");
+	}
+
+	@Test
+	@DisplayName("When author is not defined, then validation should fail.")
+	void notDefinedAuthorValidationTest() {
+		var book = Book.builder()
+		               .isbn("1234567890")
+		               .title("Title")
+		               .author("")
+		               .price(9.9)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book author must be defined.");
+	}
+
+	@Test
+	@DisplayName("When price is not defined, then validation should fail.")
+	void notDefinedPriceValidationTest() {
+		var book = Book.builder()
+		               .isbn("1234567890")
+		               .title("Title")
+		               .author("Author")
+		               .price(null)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book price must be defined.");
+	}
+
+	@Test
+	@DisplayName("When price is defined but set to zero, then validation should fail.")
+	void zeroPriceValidationTest() {
+		var book = Book.builder()
+		               .isbn("1234567890")
+		               .title("Title")
+		               .author("Author")
+		               .price(0.0)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book price must be greater than zero.");
+	}
+
+	@Test
+	@DisplayName("When price is defined but set to negative number, then validation should fail.")
+	void negativePriceValidationTest() {
+		var book = Book.builder()
+		               .isbn("1234567890")
+		               .title("Title")
+		               .author("Author")
+		               .price(-2.0)
+		               .build();
+		Set<ConstraintViolation<Book>> violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator()
+		                     .next()
+		                     .getMessage()).isEqualTo("The book price must be greater than zero.");
 	}
 }
